4aa512a304a02cc94f63f40ca238dbaae147e1f4
itamardol <132146870+itamardol@users.noreply.github.com>
2023-05-02 19:04:32 +0300

    Update tcpaggregator.py
    added # explanation

--- tcpaggregator.py
+++ tcpaggregator.py
@@ -2,6 +2,7 @@
 import sys
 from subprocess import PIPE, Popen
 
+# takes output of a bash command and returns it as the output of the function
 def cmdline(command):
     process = Popen(
         args=command,
@@ -11,10 +12,12 @@ def cmdline(command):
     )
     return process.communicate()[0]
 
+# creates file for the output
 pcap_file = sys.argv[1]
 output_file_name = pcap_file.split('.')[0]+'_streams.txt'
 output_file = open(output_file_name,'w')
 count = 0
+# main tshark stream loop, separated by the lines
 while True:
     cmd = 'tshark -r %s -z follow,tcp,ascii,%s'%(pcap_file,count)
     stream = cmdline(cmd)
